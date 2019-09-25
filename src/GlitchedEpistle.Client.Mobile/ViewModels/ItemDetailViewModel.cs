a1af7ebe75c5bdcc255807ba498e61ff996b9cc9
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-09-25 17:24:14 +0200

    Added new xamarin forms solution.

---
+++ src/GlitchedEpistle.Client.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GlitchedEpistle.Client.Mobile.Models;
+
+namespace GlitchedEpistle.Client.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
