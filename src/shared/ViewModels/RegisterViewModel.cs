771d544af6d378d2fcfe3fae40c917f672ca49ed
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-10-02 22:00:25 +0200

    Started working on registration page viewmodel.

--- src/shared/ViewModels/RegisterViewModel.cs
+++ src/shared/ViewModels/RegisterViewModel.cs
@@ -16,13 +16,82 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Prism.Events;
+using System.Windows.Input;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Commands;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.PubSubEvents;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.ViewModels
 {
     public class RegisterViewModel : ViewModel
     {
+        #region Injections
+        private readonly IEventAggregator eventAggregator;
+        #endregion
+
+        #region UI Bindings
+        private string username = string.Empty;
+        public string Username
+        {
+            get => username;
+            set => Set(ref username, value);
+        }
+
+        private string userCreationSecret = string.Empty;
+        public string UserCreationSecret
+        {
+            get => userCreationSecret;
+            set => Set(ref userCreationSecret, value);
+        }
+
+        private string password = string.Empty;
+        public string Password
+        {
+            get => password;
+            set => Set(ref password, value);
+        }
+
+        private string passwordConfirmation = string.Empty;
+        public string PasswordConfirmation
+        {
+            get => passwordConfirmation;
+            set => Set(ref passwordConfirmation, value);
+        }
+
+        private bool generatingKeys;
+        public bool GeneratingKeys
+        {
+            get => generatingKeys;
+            set => Set(ref generatingKeys, value);
+        }
+        #endregion
+
+        #region Commands
+        public ICommand CancelCommand { get; }
+        public ICommand RegisterCommand { get; }
+        public ICommand EditServerUrlCommand { get; }
+        #endregion
+
+        public RegisterViewModel(IEventAggregator eventAggregator)
+        {
+            this.eventAggregator = eventAggregator;
+
+            CancelCommand = new DelegateCommand(OnClickedCancel);
+            RegisterCommand = new DelegateCommand(OnClickedRegister);
+            EditServerUrlCommand = new DelegateCommand(_ =>
+            {
+                eventAggregator.GetEvent<ClickedConfigureServerUrlButtonEvent>().Publish();
+            });
+        }
+
+        private void OnClickedCancel(object commandParam)
+        {
+            eventAggregator?.GetEvent<LogoutEvent>()?.Publish();
+        }
+
+        private void OnClickedRegister(object commandParam)
+        {
+            // TODO: handle user registration here
+        }
     }
 }
--- src/shared/Views/RegisterPage.xaml.cs
+++ src/shared/Views/RegisterPage.xaml.cs
@@ -16,12 +16,6 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -37,7 +31,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views
 
         protected override bool OnBackButtonPressed()
         {
-            return false;// base.OnBackButtonPressed();
+            return false;
         }
     }
 }
