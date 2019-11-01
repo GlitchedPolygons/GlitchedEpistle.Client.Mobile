c876f85e78a9cddb04adeeb9458a560c2d868d00
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-11-01 11:26:49 +0100

    Implemented participant context action popup page code behind.

--- src/shared/Views/Popups/ParticipantContextMenuPopupPage.xaml.cs
+++ src/shared/Views/Popups/ParticipantContextMenuPopupPage.xaml.cs
@@ -17,23 +17,31 @@
 */
 
 using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
 using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Services.Alerts;
 using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Services.Localization;
-using Xamarin.Forms.Xaml;
-using Rg.Plugins.Popup.Pages;
-using Xamarin.Essentials;
-using Xamarin.Forms;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views.Popups
 {
+    public enum ParticipantContextAction : int
+    {
+        None = 0,
+        MakeAdmin = 1,
+        KickAndBan = 2
+    }
+    
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ParticipantContextMenuPopupPage : PopupPage
     {
         private readonly string userId;
         private readonly IAlertService alertService;
         private readonly ILocalization localization;
-        
-        // TODO: define some sort of result to give back to whoever opened this popup page
+
+        public ParticipantContextAction Result { get; set; } = ParticipantContextAction.None;
         
         public ParticipantContextMenuPopupPage(string userId)
         {
@@ -44,10 +52,10 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views.Popups
             InitializeComponent();
         }
 
-        private void CancelButton_OnClicked(object sender, EventArgs e)
+        private async void CancelButton_OnClicked(object sender, EventArgs e)
         {
-            // TODO: implement this
-            throw new NotImplementedException();
+            Result = ParticipantContextAction.None;
+            await PopupNavigation.Instance.PopAsync();
         }
         
         private void CopyButton_OnClicked(object sender, EventArgs e)
@@ -56,16 +64,16 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views.Popups
             alertService.AlertShort(localization["Copied"]);
         }
         
-        private void MakeAdmin_OnClicked(object sender, EventArgs e)
+        private async void MakeAdmin_OnClicked(object sender, EventArgs e)
         {
-            // TODO: implement this
-            throw new NotImplementedException();
+            Result = ParticipantContextAction.MakeAdmin;
+            await PopupNavigation.Instance.PopAsync();
         }
         
-        private void KickAndBan_OnClicked(object sender, EventArgs e)
+        private async void KickAndBan_OnClicked(object sender, EventArgs e)
         {
-            // TODO: implement this
-            throw new NotImplementedException();
+            Result = ParticipantContextAction.KickAndBan;
+            await PopupNavigation.Instance.PopAsync();
         }
     }
 }
