56f9f97a64ab1ea8d216aeee884dea04139d7fdb
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-11-03 23:50:53 +0100

    Prepared active convo view + vm

---
+++ src/shared/ViewModels/ActiveConvoViewModel.cs
@@ -0,0 +1,99 @@
+﻿/*
+    Glitched Epistle - Mobile Client
+    Copyright (C) 2019 Raphael Beck
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Linq;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using GlitchedPolygons.ExtensionMethods;
+using GlitchedPolygons.Services.Cryptography.Asymmetric;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Commands;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.PubSubEvents;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Services.Alerts;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Services.Factories;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Services.Localization;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.ViewModels.Interfaces;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views;
+using GlitchedPolygons.GlitchedEpistle.Client.Mobile.Views.Popups;
+using GlitchedPolygons.GlitchedEpistle.Client.Models;
+using GlitchedPolygons.GlitchedEpistle.Client.Models.DTOs;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Logging;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Settings;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Web.Convos;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Web.Users;
+using GlitchedPolygons.Services.MethodQ;
+using Prism.Events;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Mobile.ViewModels
+{
+    public class ActiveConvoViewModel : ViewModel, IOnAppearingListener
+    {
+        #region Constants
+
+        private readonly User user;
+        private readonly ILogger logger;
+        private readonly IMethodQ methodQ;
+        private readonly IUserService userService;
+        private readonly IAppSettings appSettings;
+        private readonly IAlertService alertService;
+        private readonly IUserSettings userSettings;
+        private readonly IConvoService convoService;
+        private readonly ILocalization localization;
+        private readonly IViewModelFactory viewModelFactory;
+        private readonly IConvoPasswordProvider convoPasswordProvider;
+        private readonly IEventAggregator eventAggregator;
+        private readonly IAsymmetricCryptographyRSA crypto;
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region UI Bindings
+
+        #endregion
+
+        public ActiveConvoViewModel(User user, IConvoService convoService, IConvoPasswordProvider convoPasswordProvider, IEventAggregator eventAggregator, IAsymmetricCryptographyRSA crypto, ILogger logger, IUserService userService, IUserSettings userSettings, IMethodQ methodQ, IViewModelFactory viewModelFactory, IAppSettings appSettings)
+        {
+            localization = DependencyService.Get<ILocalization>();
+            alertService = DependencyService.Get<IAlertService>();
+            
+            this.user = user;
+            this.crypto = crypto;
+            this.logger = logger;
+            this.methodQ = methodQ;
+            this.userService = userService;
+            this.appSettings = appSettings;
+            this.userSettings = userSettings;
+            this.convoService = convoService;
+            this.eventAggregator = eventAggregator;
+            this.viewModelFactory = viewModelFactory;
+            this.convoPasswordProvider = convoPasswordProvider;
+
+        }
+        
+        public void OnAppearing()
+        {
+        }
+    }
+}
