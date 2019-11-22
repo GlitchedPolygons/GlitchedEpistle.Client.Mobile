edaaf60a622577be4774a027bb02e3ced42a71a2
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-11-23 00:27:20 +0100

    Ready for the first update :>

--- src/android/Properties/AssemblyInfo.cs
+++ src/android/Properties/AssemblyInfo.cs
@@ -43,8 +43,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.1.0.0")]
-[assembly: AssemblyFileVersion("2020.1.0.0")]
+[assembly: AssemblyVersion("2020.1.0.1")]
+[assembly: AssemblyFileVersion("2020.1.0.1")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
